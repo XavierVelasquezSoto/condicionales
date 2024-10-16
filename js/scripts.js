59db96f394a5090a00a193cc29399ef06bef5df1
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2024-10-17 01:18:40 +0200

    cambio-1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,44 @@
-function areEqualNumber(numberA, numberB) {}
 
-areEqualNumber(4, 7);
+function hello( name, age) {
+        
+ if( age < 10) {
+    console.log ('Hola ' + name + ', eres menor de edad');
+  
+    } else if (age > 25){
+    console.log ('Hola ' + name + ', eres mayor');
+  }
+  
+  }
+  
+hello('xavier',8)
+hello('xavier',32)
+
+
+function number(one,two){
+    if(one > two){
+        console.log (one + ' es menor que ' + two)
+    } 
+    else (two > one); {
+        console.log (two + ' es mayor que ' + one)
+
+    }
+
+}
+
+number(27,30)
+
+
+function numberConsole(a, b, c){
+    
+    if (a < b || b < a){
+        console.log(  'es un número negativo')
+    }else if (a > b > c){
+        console.log ('es un número positivo')
+    }
+    else{
+        console.log('es cero')
+    }
+}
+
+numberConsole(5,5)
+numberConsole(5,1,5)
