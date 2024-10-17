398b742e1f1ae5132337478f7d29d279de27e145
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2024-10-17 13:27:39 +0200

    ..

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 function hello( name, age) 
 {
         
- if( age < 18) 
+ if (age < 18) 
     {
     console.log ('Hola ' + name + ', eres menor de edad');
     }
@@ -21,9 +21,9 @@ hello('xavier',32)
 
 function number(a,b)
 {
-  if(a !== b)
+  if (a !== b)
     {
-    if(a > b)
+    if (a > b)
         {
         console.log(a)   
         }
@@ -42,12 +42,12 @@ number(33,30)
 
 function numberConsole(a)
 {
-    if(a<0)
+    if (a < 0)
     {
         console.log ( ' es un número negativo');
     }
 
-    else if (a>0)
+    else if (a > 0)
     {
         console.log ( ' es un número positivo');
         
@@ -63,5 +63,27 @@ numberConsole(-5)
 numberConsole(5)
 numberConsole(0)
 
+// 
 
+function note(a,b,c)
+{
+    if (a < b)
+    {
+        console.log(' Suspenso')
+    }
 
+    else if (b > c) 
+    {
+        console.log (' Aprobado')
+    }
+
+    else if (c >= 8)
+    {
+        console.log (' Matricula de honor')
+    }
+
+}
+
+note(2,5,9)
+note(8,8,7)
+note(6,3,9)
