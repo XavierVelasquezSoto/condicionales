46bc2b49ac4ed6d8ac74b5d8e6a31ae65e0fb51a
xavier <xaviervelasquez_s@hotmail.com>
2024-10-17 18:01:22 +0200

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,89 +1,113 @@
-
-function hello( name, age) 
-{
-        
- if (age < 18) 
-    {
-    console.log ('Hola ' + name + ', eres menor de edad');
-    }
-
- else 
-    {
-    console.log ('Hola ' + name + ', eres mayor');
-    }
-  
-}
-  
-hello('xavier',8)
-hello('xavier',32)
-
-
-
-function number(a,b)
-{
-  if (a !== b)
-    {
-    if (a > b)
-        {
-        console.log(a)   
-        }
-    else    // a < b ("a" es menor que "b")
-        {
-        console.log (b)
-        }
-   
-    }  
-
+function hello(name, age) {
+  if (age >= 18) {
+    console.log('Hola ' + name + ', eres menor de edad');
+  } else {
+    console.log('Hola ' + name + ', eres mayor');
+  }
 }
 
-number(33,30)
+hello('Xavier', 8);
+hello('Jorge', 32);
 
+//
 
-
-function numberConsole(a)
-{
-    if (a < 0)
-    {
-        console.log ( ' es un número negativo');
-    }
-
-    else if (a > 0)
-    {
-        console.log ( ' es un número positivo');
-        
-    }
-    else
-    {
-        console.log (' es cero')
-    }
-
+function hightest(numberA, numberB) {
+  if (numberA > numberB) {
+    console.log(numberA);
+  } else {
+    console.log(numberB);
+  }
 }
 
-numberConsole(-5)
-numberConsole(5)
-numberConsole(0)
+hightest(40, 9);
 
-// 
+//
 
-function note(a,b,c)
-{
-    if (a < b)
-    {
-        console.log(' Suspenso')
-    }
-
-    else if (b > c) 
-    {
-        console.log (' Aprobado')
-    }
-
-    else if (c >= 8)
-    {
-        console.log (' Matricula de honor')
-    }
-
+function numberConsole(a) {
+  if (a > 0) {
+    console.log(' Es un número positivo');
+  } else if (a < 0) {
+    console.log(' Es un número negativo');
+  } else {
+    console.log(' El valor es 0');
+  }
 }
 
-note(2,5,9)
-note(8,8,7)
-note(6,3,9)
+numberConsole(3);
+numberConsole(-3);
+numberConsole(0);
+
+//
+
+function calification(numberA, numberB, numberC) {
+  const media = (numberA + numberB + numberC) / 3;
+
+  if (media > 0 && media < 5) {
+    console.log('suspenso');
+  } else if (media >= 5 && media < 8) {
+    console.log(' Aprobada');
+  } else {
+    console.log('Matricula de honor');
+  }
+}
+
+calification(8, 8, 5);
+
+//
+
+function receive(numberA, numberB, numberC) {
+  if (numberA > numberB && numberA > numberC) {
+    console.log('A es mayor');
+  } else if (numberB > numberC && numberB > numberA) {
+    console.log('B es mayor');
+  } else {
+    console.log('C es mayor');
+  }
+}
+
+receive(5, 3, 1);
+
+//
+
+function number(a) {
+  if (a % 5 === 0 && a % 3 === 0) {
+    console.log('Es divisible por 3 y por 5');
+  } else if (a % 5 === 0) {
+    console.log('Es divisible por 5');
+  } else if (a % 3 === 0) {
+    console.log('Es divisible por 3');
+  } else {
+  }
+}
+
+number(30);
+number(25);
+number(9);
+
+//
+
+function evenOdd(a) {
+  if (a % 2 === 0) {
+    console.log(' Es un número par');
+  } else {
+    console.log(' Es un número impar');
+  }
+}
+
+evenOdd(12);
+evenOdd(9);
+
+//
+
+function year(a) {
+  if ((a % 4 === 0 && a % 100 !== 0) || a % 400 === 0) {
+    console.log(' Es un año bisiesto.');
+  } else {
+    console.log(' No es un año bisiesto');
+  }
+}
+
+year(1900);
+year(2224);
+
+//
